62e7a7d3667816087a7a652365103556de155a52
mikep <mikep@dev.netmail.com>
2024-05-31 13:37:52 -0400

    added navigation to app, resoursify some strings, bottom app bar in every screen

--- app/src/main/java/dhmp/wearwise/MainActivity.kt
+++ app/src/main/java/dhmp/wearwise/MainActivity.kt
@@ -4,27 +4,55 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dhmp.wearwise.ui.screens.clothing.ClothingScreen
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import dhmp.wearwise.ui.screens.common.AppNav
+import dhmp.wearwise.ui.screens.common.AppScreens
+import dhmp.wearwise.ui.screens.common.WearWiseBottomAppBar
 import dhmp.wearwise.ui.theme.WearWiseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             WearWiseTheme {
-                ClothingScreen()
+                App()
             }
         }
     }
 }
 
+
+
+@Composable
+fun App(navController: NavHostController = rememberNavController()) {
+    Scaffold (
+        bottomBar = {
+            WearWiseBottomAppBar(
+                navOutfit = { navController.navigate(AppScreens.Outfit.name) },
+                navClothing = { navController.navigate(AppScreens.Clothing.name) },
+                navNewClothing = { navController.navigate(AppScreens.NewClothing.name) },
+                navShop = { navController.navigate(AppScreens.Shop.name) },
+            )
+        }
+    ) { innerPadding ->
+        AppNav(modifier =  Modifier.padding(innerPadding), navController = navController)
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WearWiseTheme {
-        ClothingScreen()
+//        ClothingScreen()
+//        App()
     }
 }
--- app/src/main/java/dhmp/wearwise/ui/screens/clothing/ClothingViewModel.kt
+++ app/src/main/java/dhmp/wearwise/ui/screens/clothing/ClothingViewModel.kt
@@ -23,7 +23,6 @@ class ClothingViewModel(private val garmentRepository: GarmentsRepository): View
     var showBrandFilterMenu by mutableStateOf(false)
     init {
         reset()
-        collectGarments()
     }
 
     fun reset() {
@@ -32,13 +31,12 @@ class ClothingViewModel(private val garmentRepository: GarmentsRepository): View
 
     /**
      * This function is used to get all the garments from the database
-     * 1. viewModelScope.launch is used to launch a coroutine within the viewModel lifecycle.
-     * 2. repository.getAllGarmentsStream() is used to get all the garments from the database.
-     * 3. flowOn(Dispatchers.IO) is used to change the dispatcher of the flow to IO, which is optimal for IO operations, and does not block the main thread.
-     * 4. collect is a suspending function used to collect the flow of books list, and assign the value to favoriteBooks.
-     * 5. each time the flow emits a new value, the collect function will be called with the list of books.
+     * - viewModelScope.launch is used to launch a coroutine within the viewModel lifecycle.
+     * - repository.getAllGarmentsStream() is used to get all the garments from the database.
+     * - flowOn(Dispatchers.IO) is used to change the dispatcher of the flow to IO, which is optimal for IO operations, and does not block the main thread.
+     * - each time the flow emits a new value, the collect function will be called with the list of books.
      */
-    private fun collectGarments() {
+    fun collectGarments() {
         viewModelScope.launch {
             garmentRepository.getAllGarmentsStream().flowOn(Dispatchers.IO).collect { c ->
                 _uiState.update { currentState ->
@@ -50,4 +48,6 @@ class ClothingViewModel(private val garmentRepository: GarmentsRepository): View
             garmentRepository.insertGarment(Garment())
         }
     }
+
+
 }
